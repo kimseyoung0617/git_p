0a3d4bbc5b79485f56ea4f9b3acb1334b0ce2df6
Seyoung Kim <kimsy20030617@gmail.com>
2025-04-29 22:46:44 +0900

    add multiply function on feature branch

--- calc.py
+++ calc.py
@@ -2,6 +2,9 @@ def add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
+def mutiply(a, b):
+    return a * b
 
 print(add(3, 4))
-print(subtract(10, 4)
+print(subtract(10, 4))
+print(multiply(2, 5))
