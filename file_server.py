6721da2367cd592c4bc2942a321f4dea8bffa989
Seyoung Kim <kimsy20030617@gmail.com>
2025-05-14 10:12:12 +0900

    file server and client

---
+++ file_server.py
@@ -0,0 +1,32 @@
+import socket
+import os
+
+HOST, PORT = '0.0.0.0', 9999
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as srv:
+        srv.setsockpot(socket.SOL+SOCKET, socket.SO_REUSEADDR, 1)
+        srv.bind((HOST, PORT))
+        srv.listen(1)
+        print(f'[Server] Listening on {HOST}:{PORT}')
+
+        conn, addr = srv.accept()
+        with conn:
+            print(f'[Server] Connectino from {addr}')
+            req = b''
+            while not req.endswith(b'\n'):
+                chunk = conn.recv(1)
+                if not chunk:
+                    return
+                req += chunk
+                filename = req.decode().strip().split()
+            if not os.path.isfile(filename):
+                conn.sendall(b'ERROR File not found\n')
+                return
+            data = open(filename, 'rb').read()
+            header = f'OK {len(data)}\n'.encode()
+            conn.sendall(header)
+            conn.sendall(data)
+            print(f'[Server] Sent {filename} ({len(data)} bytes)')
+
+if __name__ == '__main__':
+    main()
