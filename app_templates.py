f315b750a093817c6bdb5e629aa775b5e9080260
Seyoung Kim <kimsy20030617@gmail.com>
2025-05-21 09:54:01 +0900

    app_templates

---
+++ app_templates.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+def index():
+    return render_template('index.html', title='홈', message='환영합니다!')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=10011)
